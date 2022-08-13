f50161f215902a0451201817d4f1e03904b2e83e
Takeshi Sone <takeshi.sone@gmail.com>
2022-08-13 20:37:50 +0900

    Add Vue2 examples

--- examples/vue2module/src/App.vue
+++ examples/vue2module/src/App.vue
@@ -3,19 +3,21 @@
 </template>
 
 <script setup>
-import Flipbook from 'flipbook-vue'
+import Flipbook from 'flipbook-vue/vue2'
 const pages = [
   null,
   'images/1.jpg',
   'images/2.jpg',
   'images/3.jpg',
-  'images/4.jpg',
-  'images/5.jpg',
   'images/6.jpg',
 ]
 </script>
 
-<style scoped>
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+}
 .flipbook {
   width: 100vw;
   height: 100vh;
